70f2ba94322e7c5fb55aaba82abb31e7d97ebbf1
Alexander Iwuchukwu <iwuchukwualexander@gmail.com>
2023-08-23 23:05:55 -0700

    Monty Interpreter files

---
+++ pstr.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * f_pstr - prints the str starting at the top of the stack,
+ * followed by a new line
+ * @head: stack head
+ * @counter: line_num
+ * Return: no return
+*/
+
+
+void f_pstr(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	while (h)
+	{
+		if (h->n > 127 || h->n <= 0)
+		{
+			break;
+		}
+		printf("%c", h->n);
+		h = h->next;
+	}
+	printf("\n");
+}
---
+++ stack.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * f_stack - Thisfunction prints the top
+ * @head: stack head/
+ * @counter: line_num
+ *
+ * Return: no return
+*/
+
+
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
